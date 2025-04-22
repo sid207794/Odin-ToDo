938c9a2c959521f3d59a28c9cb87c37deec9f1a2
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-04-22 12:46:43 +0000

    Create modules for plugins, output, html, css and images

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
+};
