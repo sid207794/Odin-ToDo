4d1bbb90463b990756fbe6e70f5ceaf10c328579
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-04-25 13:05:38 +0000

    Add flatpickr

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const flatpickr = require("flatpickr");// require("flatpickr/dist/themes/material_blue.css");
 
 module.exports = {
     entry: "./src/index.js",
